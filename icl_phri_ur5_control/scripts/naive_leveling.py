b9c7bcce6e4173ae04fa0a7a20a84ddb12c1e885
Yuxiang Gao <davidgogh96@gmail.com>
2018-03-01 11:22:53 -0500

    Bug fixes

--- icl_phri_ur5_control/scripts/naive_leveling.py
+++ icl_phri_ur5_control/scripts/naive_leveling.py
@@ -4,13 +4,17 @@ from __future__ import print_function
 import numpy as np
 from time import sleep
 import sys
-import copy
+from copy import deepcopy
 
 import rospy
+import message_filters
 from std_msgs.msg import String, Header, Float64
 from geometry_msgs.msg import WrenchStamped, Vector3
 
-from utils import *
+import moveit_commander
+from geometry_msgs.msg import PoseStamped
+from moveit_msgs.msg import Grasp, GripperTranslation, PlaceLocation
+from trajectory_msgs.msg import JointTrajectoryPoint
 
 class MoveGroup:
     def __init__(self):
@@ -50,10 +54,10 @@ class MoveGroup:
         #self.pose_target = self.get_pose()
     
 
-    def set_pose_target(self, axis, value):
-        axis_dict = {'x': 0, 'y': 1, 'z': 2, 'r': 3, 'p': 4, 'y': 5}
-        print('ee: %s' % self._group.get_end_effector_link())
-        self._group.set_pose_target(axis_dict[axis], value, self._group.get_end_effector_link())
+    def set_pose_target(self, value):
+        curr_pose = deepcopy(self.get_pose())
+        curr_pose.position.z = value
+        self._group.set_pose_target(curr_pose)
     # def set_pose_target_from_array(self, pose_array):
     #     # x,y,z,qx,qy,qz,w or xyzrpy
     #     # pose_target.orientation = geometry_msgs.msg.Quaternion(*quaternion_from_euler(-1.160, 0, 3.009))
@@ -70,7 +74,7 @@ class MoveGroup:
 
     def plan_move(self):
         print("============ Generating plan")
-        self.plan = self._group.plan()
+        return self._group.plan()
 
     def go(self):
         self._group.go(wait=True)
@@ -93,33 +97,68 @@ class NaiveLeveling:
         # r = rospy.Rate(15)
         self._mg = MoveGroup()
 
-        self._current_state = self._mg.getpose.position.z
-	self._state_pub.publish(self._current_state)
+        #self._current_state = self._mg.get_pose().position.z
+        #self._state_pub.publish(self._current_state)
 
-        self._control_effort_sub = rospy.Subscriber('control_effort', Float64, self._ce_callback)
-
-        self._wrench_sub = rospy.Subscriber('icl_phri_gripper/wrench_filtered', 
-                                            WrenchStamped, 
-                                            self._wrench_callback, 
-                                            queue_size=1)
-        
+            
+        wrench_sub = message_filters.Subscriber('icl_phri_gripper/wrench_filtered', 
+                                            WrenchStamped)
+        control_effort_sub = message_filters.Subscriber('control_effort', Float64)
+        ts = message_filters.TimeSynchronizer([wrench_sub, control_effort_sub], 10)
+        ts.registerCallback(self._callback)
         print(self._mg.get_pose())
+        rospy.spin()
         #self.last_torque = None
         #self.diff = 0
         # self._mg.update_pose_target()
         #r.sleep()
 
-    def _ce_callback(self, data):
-        self._mg.shift_pose_target('z', data.data)
-        self._mg.plan_move()
-        self._mg.go()
-        self._current_state = self._mg.getpose.position.z
-	self._state_pub.publish(self._current_state)
+    def _callback(self, wrench, control_effort):
+        self._current_state = self._mg.get_pose().position.z
+        torque = wrench.wrench.torque.y
+        setpoint = 0.0
+        print("hahah")
+        if torque < 2.0:
+            self._state_pub.publish(self._current_state)
+            return
+        elif torque > 5.0:
+            setpoint = self._current_state + 5.0
+        else:
+            setpoint = self._current_state + (torque-2.0)
 
-    def _wrench_callback(self, msg):
-        self.torque = msg.wrench.torque.y
-        self._torque2setpoint(self.torque)
-        self._setpoint_pub.publish(self._setpoint)
+        print(torque, self._current_state, setpoint)
+
+        self._mg.set_pose_target(control_effort.data)
+        # if not data.data - self._mg.get_pose().position.z < 0.01:
+        self._mg.plan_move()
+
+        self._state_pub.publish(self._current_state)
+        self._setpoint_pub.publish(setpoint)
+
+        self._mg.go()
+
+    # def _ce_callback(self, data):
+    #     self._mg.set_pose_target(data.data)
+    #     # if not data.data - self._mg.get_pose().position.z < 0.01:
+    #     self._mg.plan_move()
+    #     self._mg.go()
+        
+
+    # def _wrench_callback(self, msg):
+    #     self._current_state = self._mg.get_pose().position.z
+    #     torque = msg.wrench.torque.y
+    #     setpoint = 0.0
+    #     print("hahah")
+    #     if torque < 2.0:
+    #         self._state_pub.publish(self._current_state)
+    #         return
+    #     elif torque > 5.0:
+    #         setpoint = self._current_state + 5.0
+    #     else:
+    #         setpoint = self._current_state + (torque-2.0)
+    #     print(torque, self._current_state, setpoint)
+    #     self._setpoint_pub.publish(setpoint)
+    #     self._state_pub.publish(self._current_state)
         #if self.diff > 0.1:
         #    self._mg.shift_pose_target('z', self._effort)
         #    self._mg.plan_move()
@@ -130,14 +169,6 @@ class NaiveLeveling:
         #    self._mg.go()
         #print(self.diff)  )
         #print(self._mg.pose_target.position)
-
-    def _torque2setpoint(self):
-        if diff < 2.0
-            self._setpoint = self._current_state
-        elif diff > 20.0
-            self._setpoint = self._current_state + 0.18
-        else
-            self._setpoint = self._current_state + (self.torque-2.0)*0.01
 
         
 if __name__ == '__main__':
